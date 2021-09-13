1e5067b9b904d0b2c6cecd3834d4bbc1cd4d2c86
richard.yu <richard.yu@microsoft.comwq>
2021-09-13 01:14:45 +0000

    add batch build

---
+++ sonic-scripts/batch_build.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -p [platform] -r [true|false|f|t]"
+   echo -e "\t-p [Platform], If set, will make a configure, it can be 
+                \t broadcom
+                \t marvell
+                \t mellanox
+                \t cavium
+                \t centec
+                \t nephos
+                \t innovium
+                \t p4
+                \t vs"
+   echo -e "\t-r [true|false] : If reset, need password when running"
+   exit 1 # Exit script after printing help
+}
+
+while getopts ":r:p:" opt
+do
+   case "$opt" in
+      p ) 
+        platform=${OPTARG} 
+        ;; 
+      r ) 
+        reset=${OPTARG}
+        ;;
+      ? ) 
+        helpFunction 
+        ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
+
+# Print helpFunction in case parameters are empty
+if [ -z "$platform" ] && [ -z "$reset" ]
+then
+   echo "Some or all of the parameters are empty";
+   helpFunction
+fi
+
+# Begin script in case all parameters are correct
+if [ ! -z "$reset" ]; then
+    echo make a configure reset: $reset
+    #make reset
+fi
+
+if [ ! -z "$platform" ]; then
+    echo make a configure on platform: $platform
+    #make PLATFORM=$platform configure
+fi
+
+#make target/docker-syncd-bfn.gz
+#make target/docker-teamd.gz
+#make target/docker-saiserver-brcm.gz
