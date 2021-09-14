76c5e368a054caee95f0503f47033d2abe4a5b5a
richardyu-ms <richard.yu@microsoft.com>
2021-09-13 21:12:22 -0700

    add pull docker script

--- sonic-scripts/pull_saiserver_syncd_rpc_dockers.sh
+++ sonic-scripts/pull_saiserver_syncd_rpc_dockers.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -x
 ASIC=brcm
 OS_VERSION=20201231.25
-
-docker pull acs-repo.corp.microsoft.com:5001/docker-saiserver-${ASIC}:${OS_VERSION}
-docker tag docker-saiserver-${ASIC}:${OS_VERSION} docker-saiserver-${ASIC}
-docker pull acs-repo.corp.microsoft.com:5001/docker-syncd-${ASIC}-rpc:${OS_VERSION}
-docker tag docker-syncd-${ASIC}-rpc:${OS_VERSION} docker-syncd-${ASIC}-rpc
+SONIC_REG=acs-repo.corp.microsoft.com:5001
+docker pull ${SONIC_REG}/docker-saiserver-${ASIC}:${OS_VERSION}
+docker tag  ${SONIC_REG}/docker-saiserver-${ASIC}:${OS_VERSION} docker-saiserver-${ASIC}
+docker pull ${SONIC_REG}/docker-syncd-${ASIC}-rpc:${OS_VERSION}
+docker tag ${SONIC_REG}/docker-syncd-${ASIC}-rpc:${OS_VERSION} docker-syncd-${ASIC}-rpc
