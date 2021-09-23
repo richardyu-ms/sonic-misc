7a91aabe9230d31972ad19ec1cb96e4f6489fc62
richardyu-ms <richard.yu@microsoft.com>
2021-09-23 01:12:04 -0700

    add more log in start_syncd_rpc.sh

--- sonic-scripts/DUTScript/start_syncd_rpc.sh
+++ sonic-scripts/DUTScript/start_syncd_rpc.sh
@@ -7,5 +7,8 @@ get_os_version
 docker stop syncd
 docker rm syncd
 #replace syncd with syncd-rpc
+echo "Tag [docker-syncd-rpc-${ASIC}:${OS_VERSION}]  To [docker-syncd-rpc-${ASIC}]"
+docker tag docker-syncd-rpc-${ASIC}:${OS_VERSION} docker-syncd-rpc-${ASIC}
+echo "Tag [docker-syncd-rpc-${ASIC}] to  [docker-syncd-${ASIC}:latest]"
 docker tag docker-syncd-rpc-${ASIC} docker-syncd-${ASIC}:latest
 /usr/bin/syncd.sh start
