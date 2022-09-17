47d4be55da52062e4bd2db88deda0e5c263d54eb
richardyu-ms <richard.yu@microsoft.com>
2022-09-17 06:33:10 +0000

    [DUT script]update script for pull saiserver and syncd
    Update the script for seperately pulling the docker saiserver and syncd rpc
    
    ./pull_saiserver_syncd_rpc_dockers.sh -v v2 -t syncd
    Pull docker syncd rpc
    ./pull_saiserver_syncd_rpc_dockers.sh -v v2
    Pull docker saiserver v2
    
    Signed-off-by: richardyu-ms <richard.yu@microsoft.com>

--- sonic-scripts/DUTScript/pull_saiserver_syncd_rpc_dockers.sh
+++ sonic-scripts/DUTScript/pull_saiserver_syncd_rpc_dockers.sh
@@ -4,21 +4,40 @@ DIR=$(dirname $(readlink -f "$0")) # absolute path
 get_asic
 get_os_version
 
-while getopts ":v:" args; do
+SONIC_REG=acs-repo.corp.microsoft.com:5001
+
+pull_saiserver(){
+    docker pull ${SONIC_REG}/docker-saiserver${SAI_VERSION}-${ASIC}:${OS_VERSION}
+    docker tag  ${SONIC_REG}/docker-saiserver${SAI_VERSION}-${ASIC}:${OS_VERSION} docker-saiserver${SAI_VERSION}-${ASIC}
+}
+
+pull_syncd_rpc(){
+    docker pull ${SONIC_REG}/docker-syncd-${ASIC}-rpc:${OS_VERSION}
+    docker tag ${SONIC_REG}/docker-syncd-${ASIC}-rpc:${OS_VERSION} docker-syncd-${ASIC}-rpc
+}
+
+pull_docker(){
+    if [[ x"$TARGET" =~ x"syncd" ]]; then
+        echo "Pull docker syncd rpc"
+    else
+        echo "Pull docker saiserver $SAI_VERSION"
+    fi 
+}
+
+while getopts ":v:t:" args; do
     case $args in
         v)
             SAI_VERSION=${OPTARG} 
             ;;
+        t)
+            TARGET=${OPTARG} 
+            ;;
         *)
             echo -e "\t-v [v1|v2]: saiserver version, support v1 and v2"
+            echo -e "\t-t [saiserver|syncd]: saiserver or syncd"
         ;;
     esac
 done
 
 check_sai_versions
-
-SONIC_REG=acs-repo.corp.microsoft.com:5001
-docker pull ${SONIC_REG}/docker-saiserver${SAI_VERSION}-${ASIC}:${OS_VERSION}
-docker tag  ${SONIC_REG}/docker-saiserver${SAI_VERSION}-${ASIC}:${OS_VERSION} docker-saiserver${SAI_VERSION}-${ASIC}
-docker pull ${SONIC_REG}/docker-syncd-${ASIC}-rpc:${OS_VERSION}
-docker tag ${SONIC_REG}/docker-syncd-${ASIC}-rpc:${OS_VERSION} docker-syncd-${ASIC}-rpc
+pull_docker
